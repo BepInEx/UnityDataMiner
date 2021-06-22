44471bdd7441d2d1485aee8d849cd4041b614ea7
ghorsington <neigh@coder.horse>
2021-06-22 23:08:21 +0300

    Add announcement support

--- run.py
+++ run.py
@@ -3,10 +3,19 @@ import subprocess
 import requests
 import os.path as p
 import datetime
+import string as s
 
 ANNOUNCE_DISCORD_WEBHOOK = None
-ERROR_DISCORD_WEBHOOK = "https://discord.com/api/webhooks/856947137568833556/Lvz2e5QRxG2KhrmN7xDay4WGayFyFt1z40Ev-0zBQOyKHYKz7AuwlnJKL-zLZ0Qq21ML"
+ERROR_DISCORD_WEBHOOK = None
 REPO_PATH = p.join(p.dirname(p.realpath(__file__)), "data")
+LISTING_URL = ""
+
+announce_template = s.Template(
+    """
+New Unity libraries available:
+$libs
+View all libraries at $lib_url.
+""")
 
 
 def push_discord(url, color, title, msg):
@@ -26,8 +35,23 @@ def push_discord(url, color, title, msg):
 
 
 try:
-    result = subprocess.run(["docker", "run", "--rm", "-v", f"{p.join(REPO_PATH, 'repo')}:/data",
+    repo_path = p.join(REPO_PATH, "repo")
+    unity_libs_path = p.join(repo_path, "libraries")
+    old_libs = set(os.listdir(unity_libs_path))
+
+    result = subprocess.run(["docker", "run", "--rm", "-v", f"{repo_path}:/data",
                             "-v", f"{p.join(REPO_PATH, 'config.toml')}:/app/config.toml", "-it", "unity-miner"], check=True)
+
+    new_libs = set(os.listdir(unity_libs_path))
+
+    diff = new_libs - old_libs
+    if diff:
+        push_discord(ANNOUNCE_DISCORD_WEBHOOK, "30b563", "New Unity Libs",
+                     announce_template.substitute({
+                         "libs": "\n".join([f"* {v[:-3]}" for v in sorted(diff)]),
+                         "lib_url": LISTING_URL
+                     }))
 except subprocess.CalledProcessError as ex:
     if ex.returncode != -1:
-        push_discord(ERROR_DISCORD_WEBHOOK, "bd3d3d", "Error", f"Unexpected error while running script: `{ex.returncode}`")
+        push_discord(ERROR_DISCORD_WEBHOOK, "bd3d3d", "Error",
+                     f"Unexpected error while running script: `{ex.returncode}`")
