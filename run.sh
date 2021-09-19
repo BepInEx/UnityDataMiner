6e5d201eb2131c2dbcee826efadc857c415c6d52
ghorsington <neigh@coder.horse>
2021-09-19 15:32:55 +0300

    Clean up

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run --rm -v "$(pwd)/tmp_data/repo:/data" -v "$(pwd)/tmp_data/config.toml:/app/config.toml" -it unity-miner
+docker run --rm -v "$(pwd)/data/repo:/data" -v "$(pwd)/data/config.toml:/app/config.toml" -it unity-miner
