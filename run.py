9083ed569610464e4369daf6c3463305ad675dc5
ghorsington <neigh@coder.horse>
2021-09-19 15:32:11 +0300

    run.py: move secrets to separate config

--- run.py
+++ run.py
@@ -4,11 +4,17 @@ import requests
 import os.path as p
 import datetime
 import string as s
+import configparser
 
-ANNOUNCE_DISCORD_WEBHOOK = None
-ERROR_DISCORD_WEBHOOK = None
+
 REPO_PATH = p.join(p.dirname(p.realpath(__file__)), "data")
-LISTING_URL = ""
+
+try:
+    c = configparser.ConfigParser()
+    c.read(p.join(REPO_PATH, "runner.cfg"))
+    config = c["runner"]
+except:
+    config = {}
 
 announce_template = s.Template(
     """
@@ -28,30 +34,32 @@ def push_discord(url, color, title, msg):
                 "title": title,
                 "color": int(color, 16),
                 "description": msg,
-                "timestamp": datetime.datetime.now().isoformat(),
+                "timestamp": datetime.datetime.utcnow().isoformat(),
             }
         ],
     })
 
 
 try:
+    print(f"Started at {datetime.datetime.utcnow().isoformat()}")
     repo_path = p.join(REPO_PATH, "repo")
     unity_libs_path = p.join(repo_path, "libraries")
     old_libs = set(os.listdir(unity_libs_path))
 
-    result = subprocess.run(["docker", "run", "--rm", "-v", f"{repo_path}:/data",
-                            "-v", f"{p.join(REPO_PATH, 'config.toml')}:/app/config.toml", "-it", "unity-miner"], check=True)
+    network = config.get("docker_network", None)
+    result = subprocess.run(["docker", "run", "--rm", "-v", f"--network={network}" if network else "", f"{repo_path}:/data",
+                            "-v", f"{p.join(REPO_PATH, 'config.toml')}:/app/config.toml", "unity-miner"], check=True)
 
     new_libs = set(os.listdir(unity_libs_path))
 
     diff = new_libs - old_libs
     if diff:
-        push_discord(ANNOUNCE_DISCORD_WEBHOOK, "30b563", "New Unity Libs",
+        push_discord(config.get("announce_discord_webhook", None), "30b563", "New Unity Libs",
                      announce_template.substitute({
                          "libs": "\n".join([f"* {v[:-3]}" for v in sorted(diff)]),
-                         "lib_url": LISTING_URL
+                         "lib_url": config.get("listing_url", "")
                      }))
 except subprocess.CalledProcessError as ex:
     if ex.returncode != -1:
-        push_discord(ERROR_DISCORD_WEBHOOK, "bd3d3d", "Error",
+        push_discord(config.get("error_discord_webhook", None), "bd3d3d", "Error",
                      f"Unexpected error while running script: `{ex.returncode}`")
